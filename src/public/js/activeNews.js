13457685673cb181a1e7028aed0eee0cbd1b1b69
mrjacksmith <a.a.peshnin@gmail.com>
2023-08-23 14:34:14 +0300

    with calculator and advantages

---
+++ src/public/js/activeNews.js
@@ -0,0 +1,40 @@
+const slideNews = (n) => {
+    showCurrentSlideNews(currentIndexNews = n)
+}
+
+const slidePlusNews = (n) => {
+    showCurrentSlideNews(currentIndexNews += n)
+}
+
+const showCurrentSlideNews = (n) => {
+    const dots = document.getElementsByClassName('dot-news')
+
+    if (n > 4) {
+        currentIndexNews = 1
+        n = 1
+    }
+
+    if (n < 1) {
+        currentIndexNews = 4
+        n = 4
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        const el = dots[i]
+        el.classList.remove('active')
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        if (i == (n - 1)) {
+            dots[i].classList.add('active')
+        }
+    }
+
+    difference = -(100 * (n - 1))
+    waveObjNews.style.transform = `translateX(${difference}%)`
+}
+
+let currentIndexNews = 1
+const waveObjNews = document.querySelector('#news')
+
+showCurrentSlideNews(currentIndexNews)
